5680b19817f3d863c8d8ed978283d9d3dc006cee
josoavj <josoavonjiniaina13@gmail.com>
2024-11-22 11:42:30 +0300

    feat: Initialisation de la partie UI

--- main.py
+++ main.py
@@ -11,3 +11,13 @@ SCREEN_HEIGHT = 500
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Sliding Puzzle - Choix de la grille du jeux")
 
+# UI - Interface Utilisateur
+# Initialisation des couleurs à utiliser
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (200, 200, 200)
+BLUE = (70, 130, 180)
+
+
+# Police: Modification de la police pour l'ensemble des interfaces
+font = pygame.font.SysFont("Poppins", 40)
