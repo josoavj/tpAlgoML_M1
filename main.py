d40b282c78cf7eba2d9039a836861cb11dbced87
josoavj <josoavonjiniaina13@gmail.com>
2024-11-22 11:36:54 +0300

    feat: Ajout de Pygame dans le projet

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+import pygame
+import random
+import sys
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Initialisation de PyGame
+pygame.init()
 
+# Entrée des dimensions de la fenêtre
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Sliding Puzzle - Choix de la grille du jeux")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
