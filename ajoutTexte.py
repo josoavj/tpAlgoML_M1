08843db9fb88723849f575a51eb24fd170bbcfea
josoavj <josoavonjiniaina13@gmail.com>
2024-11-22 13:13:08 +0300

    refactor: Clean code

--- ajoutTexte.py
+++ ajoutTexte.py
@@ -1,10 +1,10 @@
 # Ajout de texte pour l'interface
 
-def ajouttexte(screen, font, text, color, x, y, center=True):
-    rendered_text = font.render(text, True, color)
-    text_rect = rendered_text.get_rect()
+def ajouttexte(ecran, font, text, color, x, y, center=True):
+    texte_rendu = font.render(text, True, color)
+    bouton = texte_rendu.get_rect()
     if center:
-        text_rect.center = (x, y)
+        bouton.center = (x, y)
     else:
-        text_rect.topleft = (x, y)
-    screen.blit(rendered_text, text_rect)
+        bouton.topleft = (x, y)
+    ecran.blit(texte_rendu, bouton)
