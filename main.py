c2196ac881752b15a26e28f37b4b630cbc644664
josoavj <josoavonjiniaina13@gmail.com>
2024-11-24 16:24:53 +0300

    fix: Résolution des erreurs et bugs

--- main.py
+++ main.py
@@ -1,31 +1,16 @@
 import pygame
-import random
 
-# Initialisation de Pygame
-pygame.init()
-
-# Dimensions de la fenêtre
-SCREEN_WIDTH = 500
-SCREEN_HEIGHT = 500
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Sliding Puzzle Game")
-
-# Couleurs
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-GRAY = (200, 200, 200)
-BLUE = (70, 130, 180)
-LIGHT_GRAY = (220, 220, 220)
-LIGHT_GREEN = (4, 203, 0)
-RED = (255, 0, 0)
-
-# Police
-font = pygame.font.SysFont("Arial bold", 28)
+from afficheAlert import draw_alert_box
+from ajoutTexte import draw_text
+from config import *
+from creationGrilleRand import create_puzzle, draw_grid
+from decoupeImage import slice_image
+from tuiles import get_tile_clicked, perfomr_swap, move_tile
 
 # Charger les images
-BACKGROUND_PATH = "https://github.com/josoavj/tpAlgoML_M1/blob/master/assets/Main.png"  # Image de fond pour le menu
-IMAGE_PATH = "https://github.com/josoavj/tpAlgoML_M1/blob/master/assets/pixelcut-export (1).png"  # Image pour le puzzle
-BACKGROUND_PATH2 = "https://github.com/josoavj/tpAlgoML_M1/blob/master/assets/Main 2.png"  # Image de fond pour le menu
+BACKGROUND_PATH = "assets/Main.png"  # Image de fond pour le menu
+IMAGE_PATH = "assets/pixelcut-export (1).png"  # Image pour le puzzle
+BACKGROUND_PATH2 = "assets/Main 2.png"  # Image de fond pour le menu
 
 try:
     background_image = pygame.image.load(BACKGROUND_PATH)
@@ -74,127 +59,7 @@ move_count = 0
 swap_available = False
 show_alert = False
 swap_mode = False
-selected_tiles = []
 
-# Fonction pour découper une image en morceaux
-def slice_image(image, grid_size):
-    image = pygame.transform.scale(image, (SCREEN_WIDTH, SCREEN_HEIGHT))
-    tile_size = SCREEN_WIDTH // grid_size
-    pieces = []
-    for row in range(grid_size):
-        for col in range(grid_size):
-            x = col * tile_size
-            y = row * tile_size
-            piece = image.subsurface((x, y, tile_size, tile_size))
-            pieces.append(piece)
-    return pieces
-
-# Fonction pour dessiner du texte
-def draw_text(text, color, x, y, center=True):
-    rendered_text = font.render(text, True, color)
-    text_rect = rendered_text.get_rect()
-    if center:
-        text_rect.center = (x, y)
-    else:
-        text_rect.topleft = (x, y)
-    screen.blit(rendered_text, text_rect)
-
-# Fonction pour vérifier si une grille est résolvable
-def is_solvable(grid):
-    flat_grid = [tile for row in grid for tile in row if tile != 0]
-    inversions = 0
-    for i in range(len(flat_grid)):
-        for j in range(i + 1, len(flat_grid)):
-            if flat_grid[i] > flat_grid[j]:
-                inversions += 1
-    return inversions % 2 == 0
-
-# Fonction pour créer une grille aléatoire
-def create_puzzle(size):
-    tiles = list(range(size * size))
-    while True:
-        random.shuffle(tiles)
-        grid = [tiles[i:i + size] for i in range(0, len(tiles), size)]
-        if is_solvable(grid):
-            return grid
-
-# Fonction pour dessiner la grille du puzzle
-def draw_grid(grid, pieces, grid_size):
-    tile_size = SCREEN_WIDTH // grid_size
-    for i, row in enumerate(grid):
-        for j, tile in enumerate(row):
-            x = j * tile_size
-            y = i * tile_size
-            if tile != 0:  # Ne pas dessiner l'espace vide
-                screen.blit(pieces[tile], (x, y))
-            pygame.draw.rect(screen, GRAY, (x, y, tile_size, tile_size), 2)
-
-            #Si tuile sélectionné
-            if (i , j) in selected_tiles:
-                pygame.draw.rect(screen, BLUE, (j * tile_size, i * tile_size, tile_size, tile_size), 2)
-
-# Fonction pour déplacer les tuiles
-def move_tile(grid, direction):
-    global move_count, swap_available, show_alert
-
-    size = len(grid)
-    empty_x, empty_y = [(x, y) for x in range(size) for y in range(size) if grid[x][y] == 0][0]
-
-    moved = False
-    if direction == "UP" and empty_x < size - 1:
-        grid[empty_x][empty_y], grid[empty_x + 1][empty_y] = grid[empty_x + 1][empty_y], grid[empty_x][empty_y]
-        moved = True
-    elif direction == "DOWN" and empty_x > 0:
-        grid[empty_x][empty_y], grid[empty_x - 1][empty_y] = grid[empty_x - 1][empty_y], grid[empty_x][empty_y]
-        moved = True
-    elif direction == "LEFT" and empty_y < size - 1:
-        grid[empty_x][empty_y], grid[empty_x][empty_y + 1] = grid[empty_x][empty_y + 1], grid[empty_x][empty_y]
-        moved = True
-    elif direction == "RIGHT" and empty_y > 0:
-        grid[empty_x][empty_y], grid[empty_x][empty_y - 1] = grid[empty_x][empty_y - 1], grid[empty_x][empty_y]
-        moved = True
-
-    if moved:
-        move_count += 1
-        if chosen_swap[1] > 0 and move_count % chosen_swap[1] == 0 :
-            swap_available = True
-            show_alert = True
-
-#Fonction pour échanger les tuiles
-def perfomr_swap(puzzle, selected_tiles):
-    if len(selected_tiles) == 2:
-        pos1, pos2 = selected_tiles
-        puzzle[pos1[0]][pos1[1]], puzzle[pos2[0]][pos2[1]] = puzzle[pos2[0]][pos2[1]], puzzle[pos1[0]][pos1[1]]
-        return True
-    return False
-
-#Fonction de sélection tuiles
-def get_tile_clicked(pos, grid_size):
-    tile_size = SCREEN_WIDTH // grid_size
-    x,y = pos
-    row = y // tile_size
-    col = x // tile_size
-    return row, col
-
-
-# Fonction d'affiche d'une alerte
-def draw_alert_box():
-    box_rect = pygame.Rect(100, 150, 300, 200)
-    pygame.draw.rect(screen, GRAY, box_rect)
-    pygame.draw.rect(screen, BLACK, box_rect, 2)
-    draw_text("Vous pouvez faire un swap!", BLACK, SCREEN_WIDTH // 2, 200)
-
-    #OK
-    ok_button_rect = pygame.Rect(130, 270, 100, 40)
-    pygame.draw.rect(screen, BLUE, ok_button_rect)
-    draw_text("OK", WHITE, ok_button_rect.centerx, ok_button_rect.centery)
-
-    #Fermer
-    close_button_rect = pygame.Rect(270, 270, 100, 40)
-    pygame.draw.rect(screen, RED, close_button_rect)
-    draw_text("FERMER", WHITE, close_button_rect.centerx, close_button_rect.centery)
-
-    return ok_button_rect, close_button_rect
 
 # Fonction principale
 def main():
@@ -268,7 +133,7 @@ def main():
             elif event.type == pygame.MOUSEBUTTONDOWN and show_alert:
                 x, y = event.pos
                 if 100 <= x <= 400 and 150 <= y <= 350:
-                    #cliue sur alerte
+                    #Envoie une alerte
                     show_alert = False
                     swap_available = False
         if current_screen == "menu":
